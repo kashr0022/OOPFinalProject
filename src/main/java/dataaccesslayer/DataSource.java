5dc35dad007bb3f8b8a498b7f33ff224637aa1da
Lily S. <lilyeduu@gmail.com>
2025-08-01 04:04:09 -0400

    wip datasource

--- src/main/java/dataaccesslayer/DataSource.java
+++ src/main/java/dataaccesslayer/DataSource.java
@@ -1,13 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dataaccesslayer;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
- * @author khair
+ *	data source that follows Singleton pattern.
+ *  Grabs connection credentials from properties file. private data source constructor
+ *  Note: Login auth not implemented yet.
+ *	@author Lily Schmeer
+ *	@version 1.0
+ *	@since JDK 21.0.4
  */
 public class DataSource {
-    
+    private static Connection connection = null;
+    private static String url = null;
+    private static String username = null;
+    private static String password = null;
+
+    /**
+     * Private constructor
+     */
+    private DataSource() {
+
+    }
+
+    public static boolean authenticate(String logUsername, String logPassword) {
+//      Grab db credentials from prop file and load into variables
+        openProperties();
+
+//      directly compare with user entered credentials
+        if (logUsername.equals(username) && logPassword.equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static synchronized Connection getConnection() {
+        try {
+            if (connection == null) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(url, username, password);
+
+            } else {
+
+                System.out.println("Existing connection already exists, cannot create new one.");
+
+            }
+            // multi-catch sqlexception, classnotfoundexception
+        } catch (SQLException | ClassNotFoundException exc) {
+            Logger.getLogger(DataSource.class.getName()).log(Level.SEVERE, null, exc);
+
+        }
+        return connection;
+    }
+
+    private static void openProperties() {
+        Properties props = new Properties();
+        try (InputStream in = DataSource.class.getClassLoader().getResourceAsStream("prop/database.properties")) {
+            props.load(in);
+
+//      Trim to prevent any issues with trailing/leading whitespace in the properties file
+            url = props.getProperty("jdbc.url").trim();
+            username = props.getProperty("jdbc.username").trim();
+            password = props.getProperty("jdbc.password").trim();
+        } catch (IOException exc) {
+            Logger.getLogger(DataSource.class.getName()).log(Level.SEVERE, null, exc);
+        }
+    }
 }
